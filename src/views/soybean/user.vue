8febb7e9b76f313b63977ac76380d20d5e84fec6
tianbocheng <790209876@qq.com>
2020-03-01 13:54:01 +0800

    用户ncikName

--- src/views/soybean/user.vue
+++ src/views/soybean/user.vue
@@ -73,7 +73,7 @@
             },
             {
               label: "微信昵称",
-              prop: "nickname",
+              prop: "nickName",
               editDisplay: false,
               rules: [{
                 required: true,
